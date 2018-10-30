a164004be49bb11ff32a388297af2cd7137f6b37
tangyx <2389092255@qq.com>
2018-10-30 17:27:14 +0800

    commit first

--- netcore.blog/Controllers/ValuesController.cs
+++ netcore.blog/Controllers/ValuesController.cs
@@ -6,35 +6,57 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace netcore.blog.Controllers
 {
+    /// <summary>
+    /// CONTROLLER
+    /// </summary>
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        /// <summary>
+        /// get
+        /// </summary>
+        /// <returns></returns>
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
-
+        /// <summary>
+        /// GET/ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
+        /// <summary>
+         /// POST
+         /// </summary>
+         /// <param name="value"></param>
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
-
+        /// <summary>
+        /// PUT
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-
+        /// <summary>
+        /// DELETE
+        /// </summary>
+        /// <param name="id"></param>
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
--- netcore.blog/Startup.cs
+++ netcore.blog/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace netcore.blog
 {
@@ -19,13 +22,35 @@ namespace netcore.blog
         }
 
         public IConfiguration Configuration { get; }
-
+        /// <summary>
+        /// ConfigureServices
+        /// </summary>
+        /// <param name="services"></param>
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-        }
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info {
+                    Version = "0.01",
+                    Title="test",
+                    Description="this is test desc",
+                    Contact=new Swashbuckle.AspNetCore.Swagger.Contact { Name="names",Url="baidu.com",Email="aaaa@qq.com"}
+                });
 
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = System.IO.Path.Combine(basePath, "netcore.blog.xml");
+                c.IncludeXmlComments(xmlPath, true);
+            });
+
+
+            services.AddMvc();
+
+        }
+        /// <summary>
+        /// 配置启动引用
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -33,7 +58,12 @@ namespace netcore.blog
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");  
+            });
+           
             app.UseMvc();
         }
     }
